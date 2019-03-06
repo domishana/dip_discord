74b31b47a369b883c5c006584ffdc06e151b0cc6
Shana <shana3698shana+dominion@gmail.com>
2019-03-06 17:27:00 +0900

    ・カテゴリ一覧を出力できるようにした ・デフォルトのカテゴリ名称リストをもとにカテゴリを生成するコマンドを実装した ・チャンネルの一覧を出力できるようにした

--- arrange_guild.py
+++ arrange_guild.py
@@ -10,11 +10,13 @@ class ArrangeGuild(c.Cog):
         self._bot = bot
         self._bot.add_cog(CreateRoles())
         self._bot.add_cog(ListRoles())
+        self._bot.add_cog(Categories())
+        self._bot.add_cog(Channels())
 
 
 class CreateRoles(c.Cog):
-    @c.command(name="c_roles")
-    async def create_roles(self, ctx: c.Context):
+    @c.command(name="s_roles")
+    async def set_roles(self, ctx: c.Context):
         if discord.utils.get(ctx.guild.roles, name="Player") is not None:
             print("既に役職を生成済みのため、実行できません。")
             await ctx.send("既に役職を生成済みのため、実行できません。")
@@ -36,6 +38,41 @@ class ListRoles(c.Cog):
         paginator = c.Paginator()
         for _role in ctx.guild.roles:
             paginator.add_line('{0}:{1}'.format(_role.id, _role.name))
+        for page in paginator.pages:
+            await ctx.send(page)
+        return
+
+
+class Categories(c.Cog):
+    default_category_names = ["全体", "外交", "国内", "その他"]
+
+    @c.command(name="l_ctgs")
+    async def list_categories(self, ctx: c.Context):
+        paginator = c.Paginator()
+        for _ctg in ctx.guild.categories:
+            paginator.add_line('{0}:{1}'.format(_ctg.id, _ctg.name))
+        for page in paginator.pages:
+            await ctx.send(page)
+        return
+
+    @c.command(name="s_ctgs")
+    async def set_categories(self, ctx: c.Context):
+        for category in ctx.guild.categories:
+            await ctx.send("カテゴリ {0} を削除しました。".format(category.name))
+            await category.delete()
+        await ctx.send("既存のカテゴリを全て削除しました。")
+
+        for category in Categories.default_category_names:
+            created_category = await ctx.guild.create_category(category)
+            await ctx.send("カテゴリ {0} を作成しました。".format(created_category.name))
+
+
+class Channels(c.Cog):
+    @c.command(name="l_chs")
+    async def list_channels(self, ctx: c.Context):
+        paginator = c.Paginator()
+        for _ch in ctx.guild.channels:
+            paginator.add_line('{0}:{1}'.format(_ch.id, _ch.name))
         for page in paginator.pages:
             await ctx.send(page)
         return
