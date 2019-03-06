99b23c167bd0ba3d4e4d21f46bc37d75b1e8eacc
Shana <shana3698shana+dominion@gmail.com>
2019-03-06 23:31:55 +0900

    Countryオブジェクトにアルファベット略称を追加

--- country.py
+++ country.py
@@ -3,9 +3,10 @@ import discord
 
 
 class Country:
-    def __init__(self, _name, _colour_code, _short=None):
+    def __init__(self, _name, _colour_code, _short=None, _alphabet=None):
         self._name = _name
         self._short = _short
+        self._alphabet = _alphabet
         self._colour_code = _colour_code
 
     def get_name(self):
@@ -17,13 +18,16 @@ class Country:
     def get_colour(self):
         return discord.Colour(self._colour_code)
 
+    def get_alphabet(self):
+        return self._alphabet
 
-austria = Country("オーストリア", 0xad1457, _short="墺")
-england = Country("イギリス", 0x9B59B6, _short="英")
-france = Country("フランス", 0x3498db, _short="仏")
-germany = Country("ドイツ", 0xe67e22, _short="独")
-italy = Country("イタリア", 0x2ecc71, _short="伊")
-russia = Country("ロシア", 0xE7E9F3, _short="露")
-turkey = Country("トルコ", 0xf1c40f, _short="土")
+
+austria = Country("オーストリア", 0xad1457, _short="墺", _alphabet="A")
+england = Country("イギリス", 0x9B59B6, _short="英", _alphabet="E")
+france = Country("フランス", 0x3498db, _short="仏", _alphabet="F")
+germany = Country("ドイツ", 0xe67e22, _short="独", _alphabet="G")
+italy = Country("イタリア", 0x2ecc71, _short="伊", _alphabet="I")
+russia = Country("ロシア", 0xE7E9F3, _short="露", _alphabet="R")
+turkey = Country("トルコ", 0xf1c40f, _short="土", _alphabet="T")
 
 country_list = [austria, england, france, germany, italy, russia, turkey]
